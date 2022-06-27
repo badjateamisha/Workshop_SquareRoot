a8d8dcfcf95b77baefe631c6a3b1348c54ed3393
badjateamisha <amishabadjate175@gmail.com>
2022-06-27 09:38:01 +0530

    Find square root of a number

---
+++ SquareRoot/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ public class SquareRoot
+{
+    public static void Main(String[] args)
+    {
+        double c = 16;
+        double epsilon = 1e-15;
+        double t = c;
+
+        while(Math.Abs(t- c/t) > epsilon*t)
+        {
+            t = (c / t + t)/ 2.0;
+        }
+
+        Console.WriteLine("Value =" +t);
+
+    }
+    
+}
